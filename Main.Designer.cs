2cde616a311b0d2f013d3149fd7509096036a51d
ArturBlazejczyk <34420486+ArturBlazejczyk@users.noreply.github.com>
2023-02-22 19:57:57 +0100

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace HRBase
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHire = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnFire = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dgvList = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnHire
+            // 
+            this.btnHire.BackColor = System.Drawing.Color.LightGreen;
+            this.btnHire.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnHire.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnHire.Location = new System.Drawing.Point(212, 12);
+            this.btnHire.Name = "btnHire";
+            this.btnHire.Size = new System.Drawing.Size(128, 32);
+            this.btnHire.TabIndex = 0;
+            this.btnHire.Text = "Zatrudnij";
+            this.btnHire.UseVisualStyleBackColor = false;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnEdit.Location = new System.Drawing.Point(346, 12);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(128, 32);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Edytuj";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnFire
+            // 
+            this.btnFire.BackColor = System.Drawing.Color.Tomato;
+            this.btnFire.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnFire.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnFire.Location = new System.Drawing.Point(480, 12);
+            this.btnFire.Name = "btnFire";
+            this.btnFire.Size = new System.Drawing.Size(128, 32);
+            this.btnFire.TabIndex = 2;
+            this.btnFire.Text = "Zwolnij";
+            this.btnFire.UseVisualStyleBackColor = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(667, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // dgvList
+            // 
+            this.dgvList.BackgroundColor = System.Drawing.Color.White;
+            this.dgvList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvList.Location = new System.Drawing.Point(12, 51);
+            this.dgvList.Name = "dgvList";
+            this.dgvList.Size = new System.Drawing.Size(776, 397);
+            this.dgvList.TabIndex = 4;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 460);
+            this.Controls.Add(this.dgvList);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnFire);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnHire);
+            this.Name = "Main";
+            this.Text = "HR Base";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHire;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnFire;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dgvList;
+    }
+}
+
