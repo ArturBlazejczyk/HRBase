701aeeda2a7fdf0c552e96aa998b8c4bf0f6ddd1
ArturBlazejczyk <34420486+ArturBlazejczyk@users.noreply.github.com>
2023-02-22 20:27:24 +0100

    created Employee class

---
+++ Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HRBase
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime HireDate { get; set; }
+        public DateTime FireDate { get; set; }
+        public decimal Wage { get; set; }
+        public string Remarks { get; set; }
+    }
+}
