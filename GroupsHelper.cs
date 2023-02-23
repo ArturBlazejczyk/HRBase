838ffd46551729760981b9af0251646dbb4270b8
ArturBlazejczyk <34420486+ArturBlazejczyk@users.noreply.github.com>
2023-02-23 08:07:04 +0100

    class adjustment to the project

--- GroupsHelper.cs
+++ GroupsHelper.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
-namespace StudentsDiary
+namespace HRBase
 {
     public class GroupsHelper
     {
@@ -14,9 +14,7 @@ namespace StudentsDiary
             return new List<Group>()
             {
                 new Group() { Id = 0, Name = defualtName},
-                new Group() { Id = 1, Name = "Grupa 1"},
-                new Group() { Id = 2, Name = "Grupa 2"},
-                new Group() { Id = 3, Name = "Grupa 3"},
+                new Group() { Id = 1, Name = "Zatrudniony"},
             };
         }
 
